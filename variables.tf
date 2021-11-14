c4d919cf8e01781f8455118d41bc2a2bccf7e516
ailec0623 <2523017180@qq.com>
2021-11-14 16:42:48 -0500

    vpc and base

---
+++ variables.tf
@@ -0,0 +1,28 @@
+variable "vpc_cidr_block" {
+  description = "CIDR for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "domain" {
+  description = "Domain"
+  type        = string
+  default     = "csye7200.xyz"
+}
+
+variable "AMI_owner" {
+  description = "AMI owner"
+  type        = string
+  default     = "582828740832"
+}
+
+variable "key_pair_name" {
+  description = "key pair name"
+  type        = string
+  default     = "csye7200"
+}
+
+variable "kafka_AMI_name" {
+  description = "kafka AMI name"
+  type        = string
+}
